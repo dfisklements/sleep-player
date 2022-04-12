de3de4307bc2303c716249fcaeafd14e4c93cb05
Dan <dfisklements@users.noreply.github.com>
2022-04-12 13:49:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo mkdir /media/sleep_media
+sudo chown `whoami` /media/sleep_media
 sudo cp sleep_player.service /lib/systemd/system/
 sudo systemctl daemon-reload 
 sudo systemctl enable sleep_player.service 
