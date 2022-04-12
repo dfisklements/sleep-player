a2a8ad2a41b4c21259a49892b8195a63fa98e174
Dan <dfisklements@users.noreply.github.com>
2022-04-12 13:50:56 -0400

    Update install.sh
    added media dir to install

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Create media directory
 sudo mkdir /media/sleep_media
-sudo chown `whoami` /media/sleep_media
+sudo chown $USER:$USER /media/sleep_media
+
+# Install service
 sudo cp sleep_player.service /lib/systemd/system/
 sudo systemctl daemon-reload 
 sudo systemctl enable sleep_player.service 
